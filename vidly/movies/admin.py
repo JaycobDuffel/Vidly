f596a7d2e44302289092de042cf7875678fa449e
Jaycob Duffel <jbdcoding@gmail.com>
2020-10-05 10:57:00 -0700

    Create movies app

--- vidly/movies/admin.py
+++ vidly/movies/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Genre, Movie
 
-admin.site.register(Genre)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+admin.site.register(Genre, GenreAdmin)
 admin.site.register(Movie)
